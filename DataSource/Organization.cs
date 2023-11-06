700c1cd75231035485bdb99c5016671c26ed0265
Lifebook727 <Lifebook727@DESKTOP-DO4UU49>
2023-11-06 18:13:03 +0100

    Add project files.

---
+++ DataSource/Organization.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Organisations.DataSource
+{
+    internal class Organization
+    {
+        int index;
+        string organizationId;
+        string name;
+        string website;
+        string country;
+        string description;
+        int founded;
+        string industry;
+        int numberOfEmployees;
+
+        public Organization(int index, string organizationId, string name, string website, string country, string description, int founded, string industry, int numberOfEmployees)
+        {
+            this.index = index;
+            this.organizationId = organizationId;
+            this.name = name;
+            this.website = website;
+            this.country = country;
+            this.description = description;
+            this.founded = founded;
+            this.industry = industry;
+            this.numberOfEmployees = numberOfEmployees;
+        }
+
+        public int Index { get => index; set => index = value; }
+        public string OrganizationId { get => organizationId; set => organizationId = value; }
+        public string Name { get => name; set => name = value; }
+        public string Website { get => website; set => website = value; }
+        public string Country { get => country; set => country = value; }
+        public string Description { get => description; set => description = value; }
+        public int Founded { get => founded; set => founded = value; }
+        public string Industry { get => industry; set => industry = value; }
+        public int NumberOfEmployees { get => numberOfEmployees; set => numberOfEmployees = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Console_Organisations.DataSource;
+
+namespace Console_Organisations
+{
+    internal class Program
+    {
+        static List<Organization> organizations100 = new List<Organization>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(".\\DataSource\\organizations-100.csv");
+
+            sr.ReadLine();
+            
+            while (!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(",");
+
+                Organization dataRow = new Organization(
+                    int.Parse(line[0]),
+                    line[1],
+                    line[2],
+                    line[3],
+                    line[4],
+                    line[5],
+                    int.Parse(line[6]),
+                    line[7],
+                    int.Parse(line[8])
+                    );
+
+                organizations100.Add( dataRow );   
+            }
+
+            sr.Close();
+
+            StreamReader sr1 = new StreamReader(".\\DataSource\\organizations-100000");
+
+            sr1.ReadLine();
+
+            while (!sr1.EndOfStream)
+            {
+                string[] line1 = sr.ReadLine().Split(",");
+
+                Organization datarow2 = new Organization(
+                    int.Parse(line1[0]),
+                    line1[1],
+                    line1[2],
+                    line1[3],
+                    line1[4],
+                    line1[5],
+                    int.Parse(line1[6]),
+                    line1[7],
+                    int.Parse(line1[8])
+                    );
+            }
+
+            sr1.Close();
+
+            List<Organization> foundedIn2021 =  organizations100.Where(x => x.Founded == 2012).ToList();
+
+            foundedIn2021.ForEach(x => Console.WriteLine(x.Name));
+
+        }
+    }
+}
